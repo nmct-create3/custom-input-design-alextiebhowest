ea2b84c81a01959be3b6c1fc943dde24c6b973bb
Alexandegit config --global user.name alexander.tiebergijn <alexander.tiebergijn@student.howest.be>
2023-09-27 15:28:23 +0200

    initial commit

--- script/app.js
+++ script/app.js
@@ -1,4 +1,20 @@
 const handlePasswordSwitcher = function () {
+    const passwordOptions = ['password', 'text'];
+    const passwordInput = document.querySelector('.js-password-input');
+    const passwordToggle = document.querySelector('.js-password-toggle-checkbox');
+    const show = document.querySelector(".c-password-toggle__symbol--show");
+    const hide = document.querySelector(".c-password-toggle__symbol--hide");
+
+    passwordToggle.addEventListener('change', function () {
+        passwordInput.type = passwordOptions[+this.checked];
+        if(this.checked){
+            show.style.display = 'none';
+            hide.style.display = 'block';
+        }else{
+            show.style.display = 'block';
+            hide.style.display = 'none';
+        }
+    });
     console.log("handlePasswordSwitcher executed");
 }
 
